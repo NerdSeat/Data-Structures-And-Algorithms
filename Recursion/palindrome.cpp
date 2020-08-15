fce745c4ebabaa8aaf1f5ea48c277f1185f388d3
Blisterface <josephblister@gmail.com>
2020-08-15 17:46:57 +0200

    Created a program to determine whether a string is a palindrome or not

---
+++ Recursion/palindrome.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+bool isPalindrome(std::string str)
+{
+    bool palindrome = false;
+    if (str.length() <= 1)
+        palindrome = true;
+    else if (str.front() == str.back())
+        palindrome = isPalindrome(str.substr(1, str.length() - 2));
+    return palindrome;
+}
+
+int main()
+{
+    std::vector<std::string> vecOfPalindromes = {"deed", "markram", "tut tut","11011","0021200","213132"};
+    for (auto x : vecOfPalindromes)
+    {
+        if (isPalindrome(x))
+            std::cout << x << " is a palindrome\n";
+        else
+            std::cout << x << " is not a palindrome\n";
+    }
+
+    return 0;
+}
